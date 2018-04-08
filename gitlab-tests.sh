216fd4df567bb1b94cf656dd0a69f24dc7000c44
ElFryskai <elfryskai@gmail.com>
2018-04-08 16:32:58 +0200

    [MODIFY] gitlab ci

--- gitlab-tests.sh
+++ gitlab-tests.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-if [ $1 -eq 1 ]; then
+if [ $1 == 1 ]; then
 	OUTPUT="$(./fillit)"
 	if [ "${OUTPUT}" == "usage: ./fillit source_file" ]; then
-		echo "OK"
+		exit 0
 	else
-		echo "FAIL"
+		exit 1
 	fi
 
-elif [ $1 -eq 2 ]; then
+elif [ $1 == 2 ]; then
 	OUTPUT="$(./fillit tests/totor)"
 	if [ "${OUTPUT}" == "error" ]; then
-		echo "OK"
+		exit 0
 	else
-		echo "FAIL"
+		exit 1
 	fi
 
-elif [ $1 -eq 3 ]; then
+elif [ $1 == 3 ]; then
 	OUTPUT="$(./fillit tests/test1)"
 	if [ "${OUTPUT}" == "122.\n122.\n1...\n1..." ]; then
-		echo "OK"
+		exit 0
 	else
-		echo "FAIL"
+		exit 1
 	fi
 
-elif [ $1 -eq 4 ]; then
+elif [ $1 == 4 ]; then
 	OUTPUT="$(./fillit tests/test1 tests/test2)"
 	if [ "${OUTPUT}" == "usage: ./fillit source_file" ]; then
-		echo "OK"
+		exit 0
 	else
-		echo "FAIL"
+		exit 1
 	fi
 fi
