d76fb15375114d55be69c747dd9cbe1b39db9a93
ElFryskai <elfryskai@gmail.com>
2018-04-09 09:19:07 +0200

    [ADD] finder

--- srcs/find.c
+++ srcs/find.c
@@ -6,7 +6,7 @@
 /*   By: groussel <groussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 03:52:17 by groussel          #+#    #+#             */
-/*   Updated: 2018/04/09 08:48:38 by groussel         ###   ########.fr       */
+/*   Updated: 2018/04/09 09:18:59 by groussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 ** TODO:	[ ] Start at the top left
 **			[ ] If you can't put, x++
 **			[ ] If space between first and second shape, x = 0 and y++
-**			[ ] If still impossible, backtracking
-			https://medium.com/@andreaiacono/backtracking-explained-7450d6ef9e1a
-			https://www.wikiwand.com/fr/Algorithme_de_remplissage_par_diffusion
-			[ ] If still impossible (shape reach border), add 1*1 to the map
+**			[ ] If still impossible, backtracking (reser letters, pass to next shape)
+**			https://medium.com/@andreaiacono/backtracking-explained-7450d6ef9e1a
+**			https://www.wikiwand.com/fr/Algorithme_de_remplissage_par_diffusion
+**			[ ] If still impossible (shape reach border), add 1*1 to the map
 **
 ** FUNC:	exit
 **			open / close
